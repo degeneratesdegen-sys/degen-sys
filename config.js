0bdfc329460bfcd8c798b6e0b2c609399acd7aec
degeneratesdegen-sys <degeneratesdegen@gmail.com>
2026-01-20 22:52:02 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWtso6QR1oyXs6b5aVXjf9Oi-XsCipeTg",
+  authDomain: "vaultpro-90a00.firebaseapp.com",
+  projectId: "vaultpro-90a00",
+  storageBucket: "vaultpro-90a00.firebasestorage.app",
+  messagingSenderId: "802795035216",
+  appId: "1:802795035216:web:0bf0d3efa72e5bc8004f55"
+};
+
+export default firebaseConfig;
